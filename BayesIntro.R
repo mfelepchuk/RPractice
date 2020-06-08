c1bd379c952d73bac446012e7a37c90e43533044
Mark Felepchuk <mfelepchuk@gmail.com>
2020-06-08 15:13:18 -0400

    Update BayesIntro.R

--- BayesIntro.R
+++ BayesIntro.R
@@ -15,5 +15,14 @@ bayes <- function(sz, iterations, target){
     y[i] <- sum(rbinom(sz,1,x[i]))
   }
   z = x[y==target]
-  hist(z, breaks = 30, xlim=c(0,1), col = 'lightblue2')
+  h = hist(z, breaks = 30, xlim=c(0,1), col = 'lightblue2')
+  h
+  return(list("x" = x, "y" = y, "z" = z, "hist" = h))
 }
+
+
+#what is the probability that sign up rate between 30% and 40%
+sum(e$z <.4 & e$z >.3)/length(e$z)
+
+#Maximum Likelihood Estimator:
+median(e$z)
